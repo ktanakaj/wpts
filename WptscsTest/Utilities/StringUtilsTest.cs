de96d448e1870c79b0f9797b5fa6d1262bbbc8cc
honeplus <honeplus@users.osdn.me>
2010-06-26 17:42:11 +0000

    NUnitテストプロジェクトを作成 refs #9
    git-svn-id: https://hone.server-on.net/svn/wpts/trunk@47 6be454e3-df08-4fb2-8a68-9692ddc920da

---
+++ WptscsTest/Utilities/StringUtilsTest.cs
@@ -0,0 +1,43 @@
+﻿// ================================================================================================
+// <summary>
+//      StringUtilsのテストクラスソース。</summary>
+//
+// <copyright file="StringUtilsTest.cs" company="honeplusのメモ帳">
+//      Copyright (C) 2010 Honeplus. All rights reserved.</copyright>
+// <author>
+//      Honeplus</author>
+// ================================================================================================
+
+namespace Honememo.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// StringUtilsのテストクラスです。
+    /// </summary>
+    [TestFixture]
+    public class StringUtilsTest
+    {
+        /// <summary>
+        /// DefaultStringメソッドテストケース。
+        /// </summary>
+        [Test]
+        public void TestDefaultString()
+        {
+            // 引数一つ
+            Assert.AreEqual(StringUtils.DefaultString(null), String.Empty);
+            Assert.AreEqual(StringUtils.DefaultString(String.Empty), String.Empty);
+            Assert.AreEqual(StringUtils.DefaultString(" "), " ");
+            Assert.AreEqual(StringUtils.DefaultString("null以外の文字列"), "null以外の文字列");
+
+            // 引数二つ
+            Assert.AreEqual(StringUtils.DefaultString(null, "初期値"), "初期値");
+            Assert.AreEqual(StringUtils.DefaultString(String.Empty, "初期値"), String.Empty);
+            Assert.AreEqual(StringUtils.DefaultString(" ", "初期値"), " ");
+            Assert.AreEqual(StringUtils.DefaultString("null以外の文字列", "初期値"), "null以外の文字列");
+        }
+    }
+}
