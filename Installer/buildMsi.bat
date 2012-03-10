1a8b1624689485944405ca8d7d2a836e6ecaff37
honeplus <honeplus@users.osdn.me>
2012-03-10 06:56:53 +0000

    WiX用のインストーラ設定とビルドバッチを改良, リリースに向けてドキュメントを整備 refs #50
    git-svn-id: https://hone.server-on.net/svn/wpts/trunk@212 6be454e3-df08-4fb2-8a68-9692ddc920da

--- Installer/buildMsi.bat
+++ Installer/buildMsi.bat
@@ -11,7 +11,7 @@ rem
 setlocal
 
 rem WiXインストール先フォルダ（PATHが設定されていない場合）
-set PATH=C:\Program Files (x86)\Windows Installer XML v3.5\bin;%PATH%
+set PATH=C:\Program Files (x86)\Windows Installer XML v3.5\bin;C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin;C:\Program Files\Microsoft SDKs\Windows\v7.0\Samples\sysmgmt\msi\scripts;%PATH%
 
 rem WiXビルドファイル
 set WXS_FILE=Wptscs.wxs
@@ -19,10 +19,12 @@ set WXL_JAJP_FILE=Wptscs.ja-jp.wxl
 set WXL_ENUS_FILE=Wptscs.en-us.wxl
 
 rem MSIインストーラ
-set MSI_INSTALLER=wptscs120.msi
+set MSI_INSTALLER=setup.msi
 
-rem 中間ファイル（ビルドファイル名から生成）
+rem 中間ファイル（上記ファイル名から生成）
 for /f "delims=" %%F in ("%WXS_FILE%") do set WIXOBJ_FILE=%%~nF.wixobj
+for /f "delims=" %%F in ("%MSI_INSTALLER%") do set MSI_INSTALLER_JAJP=%%~nF-ja.msi
+for /f "delims=" %%F in ("%MSI_INSTALLER%") do set MST_JAJP=%%~nF-ja.mst
 
 rem バッチのフォルダに移動
 cd /d %~dp0
@@ -32,11 +34,33 @@ candle.exe -out "%WIXOBJ_FILE%" "%WXS_FILE%"
 set RET=%ERRORLEVEL%
 if not %RET% == 0 endlocal & exit /b %RET%
 
-rem リンク
+rem リンク（英語版インストーラ）
 light.exe -ext WixUIExtension -ext WixNetFxExtension ^
-	-cultures:ja-jp,en-us ^
-	-loc "%WXL_JAJP_FILE%" -loc "%WXL_ENUS_FILE%" ^
+	-cultures:en-us -loc "%WXL_ENUS_FILE%" ^
 	-out "%MSI_INSTALLER%" "%WIXOBJ_FILE%"
+set RET=%ERRORLEVEL%
+if not %RET% == 0 endlocal & exit /b %RET%
+
+rem リンク（日本語版インストーラ）
+light.exe -ext WixUIExtension -ext WixNetFxExtension ^
+	-cultures:ja-jp -loc "%WXL_JAJP_FILE%" ^
+	-out "%MSI_INSTALLER_JAJP%" "%WIXOBJ_FILE%"
+set RET=%ERRORLEVEL%
+if not %RET% == 0 endlocal & exit /b %RET%
+
+rem 言語トランスファー情報作成
+msitran -g "%MSI_INSTALLER%" "%MSI_INSTALLER_JAJP%" "%MST_JAJP%"
+set RET=%ERRORLEVEL%
+if not %RET% == 0 endlocal & exit /b %RET%
+
+rem マルチランゲージインストーラを作成
+rem msidb -d "%MSI_INSTALLER%" -r "%MST_JAJP%"
+rem set RET=%ERRORLEVEL%
+
+WiSubStg.vbs "%MSI_INSTALLER%" "%MST_JAJP%" 1041
+set RET=%ERRORLEVEL%
+if not %RET% == 0 endlocal & exit /b %RET%
+WiLangId.vbs "%MSI_INSTALLER%" Package 1033,1041
 set RET=%ERRORLEVEL%
 
 endlocal & exit /b %RET%
