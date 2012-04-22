75c6c23b34fcffb166415978a1328bd727f98179
honeplus <honeplus@users.osdn.me>
2012-04-22 02:21:15 +0000

    #28149 テンプレート名が空の不正なテンプレート呼び出しで処理が落ちていたのを修正, Ver1.21リリース用にビルドファイル等を更新
    git-svn-id: http://svn.osdn.net/svnroot/wptscs/trunk@37 7cc79d57-4d93-40a1-83d5-ec7b38613dec

--- Wptscs/Parsers/MediaWikiTemplateParser.cs
+++ Wptscs/Parsers/MediaWikiTemplateParser.cs
@@ -125,7 +125,9 @@ namespace Honememo.Wptscs.Parsers
             }
 
             // 終了条件でループを抜けていない場合、解析失敗
-            if (lastIndex < 0)
+            // テンプレート名が無い場合も解析失敗
+            // ※ 内部リンクの場合ありえるが、テンプレートの場合2012年現在無いと認識されない
+            if (lastIndex < 0 || String.IsNullOrWhiteSpace(article.ToString()))
             {
                 return false;
             }
